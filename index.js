26d0004354f26bc190caf853167b553c7c4253c7
parkermuir <parker.muir@gmail.com>
2018-06-05 21:23:18 -0400

    Clicking username shows timeline. Min req complete.

--- index.js
+++ index.js
@@ -1,34 +1,67 @@
 
-    var renderTweets = function(){
-        var $tweets = $('.tweets')
-        $tweets.html('');
-        var index = streams.home.length - 1;
-        
-        while(index >= 0){
-            var tweet = streams.home[index];
-            var $tweet = $('<div class="tweet-container"></div>');
-            var $tweet_text = $('<p class="tweet-text"></p>').text(tweet.message);
-            var $timestamp = $('<time datetime="YYYY-MM-DD"></time>').text( '- ' + moment(tweet.created_at).fromNow());
-            $timestamp.addClass("timestamp");
-            var $username = $('<div class="username"></div>').text('@' + tweet.user);
+// var renderTweets = function(){
+//     var $tweets = $('.tweets')
+//     $tweets.html('');
+//     var index = streams.home.length - 1;
+    
+//     while(index >= 0){
+//         var tweet = streams.home[index];
+//         var $tweet = $('<div class="tweet-container"></div>');
+//         var $tweet_text = $('<p class="tweet-text"></p>').text(tweet.message);
+//         var $timestamp = $('<time datetime="YYYY-MM-DD"></time>').text( '- ' + moment(tweet.created_at).fromNow());
+//         $timestamp.addClass("timestamp");
+//         var $username = $('<div class="username"></div>').text('@' + tweet.user);
 
-            $tweet.append($username, $tweet_text, $timestamp);
-            $tweet.appendTo($tweets);
-            index -= 1;
-            }
-        }
+//         $tweet.append($username, $tweet_text, $timestamp).appendTo($tweets);
+//         index -= 1;
+//         }
+//     }
 
-    $(document).ready(function(){
-        
-    renderTweets();
-
-    $('#update-btn').on('click', function() {
-            renderTweets();
-        })
-
-    var usernameClickHandler = function() {
-        //clear streams.home and show streams.users.username
-        //display that user's specific timeline of tweets on click
+var renderTweets = function(target){
+    if (target === 'home') {
+        target = streams.home;
+    } else {
+        target = streams.users[target]
     }
 
+    var $tweets = $('.tweets')
+    $tweets.html('');
+    var index = target.length - 1;
+    
+    while(index >= 0){
+        var tweet = target[index];
+        var $tweet = $('<div class="tweet-container"></div>');
+        var $tweet_text = $('<p class="tweet-text"></p>').text(tweet.message);
+        var $timestamp = $('<time datetime="YYYY-MM-DD"></time>').text( '- ' + moment(tweet.created_at).fromNow());
+        $timestamp.addClass("timestamp");
+        var $username = $('<div class="username"></div>').text('@' + tweet.user);
+
+        $tweet.append($username, $tweet_text, $timestamp).appendTo($tweets);
+        index -= 1;
+        }
+    }
+
+$(document).ready(function(){
+    renderTweets('home');
+
+    $('#update-btn').on('click', function() {
+        renderTweets('home');
+        });
 });
+
+$(document).ready(function(){
+    $('.username').on('click', function() {
+        var $clickee = $(this).text().slice(1);
+        renderTweets($clickee);
+        $('.tweets').prepend($('<h1>' + $clickee + '\'s Profile </h1>'));
+        $('footer').remove();
+        $('#update-btn').remove();
+    });
+});
+
+    
+
+// var usernameClickHandler = function() {
+//     //clear streams.home and show streams.users.username
+//     //display that user's specific timeline of tweets on click
+// }
